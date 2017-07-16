3321170ffbd321668d918794ada86e0377e938ee
PynutCommander <ahmedsamikhalil1993@gmail.com>
2017-07-16 04:11:31 +0400

    fix(Debuffs): Added new script Debuffmanager.cs and Modified BallProperties[2nd iteration].
    testing with the new Debuff manager which would effect the player if he picks it up with various different buffs. the current implemented one doubles the size of the user for 5 seconds. which should make it harder for him if we place it in small platforms. this is only the first iteration. we have to set a template for it.
    Modified ball properties. we do not need to put it on an object any more. we pass a gameObject to it in order to do various operations on it. thats how i did the double size. i got the object i.e the player game object when he touched the onTrigger of the debuff. passed it to the ballproperties. saved all the different variables. i.e scale, location, mass. and then performed the double size operation by using the new modifyscale() function. also added 3 new variables. scaleX scaleY and scaleZ. which effect the size of the object., now the ball properties has Mass, drag, angular drag, scale x y z and transform x y z. we can do many different operations with these values.

--- Assets/Scripts/Ball/BallProperties.cs
+++ Assets/Scripts/Ball/BallProperties.cs
@@ -12,11 +12,16 @@ public class BallProperties : MonoBehaviour {
 	private float B_UP_angularDrag = 0;
 
 	//Transform Variables.
-	private float B_UP_scaleX = 0;
-	private float B_UP_scaleY = 0;
-	private float B_UP_scaleZ = 0;
+	private float B_UP_TransformX = 0;
+	private float B_UP_TransformY = 0;
+	private float B_UP_TransformZ = 0;
 
+	//Scale Variables
+	private float B_UP_ScaleX = 0;
+	private float B_UP_ScaleY = 0;
+	private float B_UP_ScaleZ = 0;
 
+	GameObject CurrentGameObject;
 	private Rigidbody ballRigidBody;
 	private Transform ballTransform;
 
@@ -25,37 +30,53 @@ public class BallProperties : MonoBehaviour {
 		initializeMaindictionaryProperties ();
 	}
 
-	void backUpAllStats(){
+	public void setGameObject(GameObject incomingGameObject){
+		CurrentGameObject = incomingGameObject;
+	}
+
+	public void backUpAllStats(){
 		B_UP_mass = MaindictionaryProperties ["Mass_Rigid"];
 		B_UP_drag = MaindictionaryProperties ["drag_Rigid"];
 		B_UP_angularDrag = MaindictionaryProperties ["AngularDrag_Rigid"];
 
-		B_UP_scaleX = MaindictionaryProperties ["ScaleX_Transform"];
-		B_UP_scaleY = MaindictionaryProperties ["ScaleY_Transform"];
-		B_UP_scaleZ = MaindictionaryProperties ["ScaleZ_Transform"];
+		B_UP_TransformX = MaindictionaryProperties ["x_Transform"];
+		B_UP_TransformY = MaindictionaryProperties ["y_Transform"];
+		B_UP_TransformZ = MaindictionaryProperties ["z_Transform"];
+
+		B_UP_ScaleX = MaindictionaryProperties ["x_Scale"];
+		B_UP_ScaleY = MaindictionaryProperties ["y_Scale"];
+		B_UP_ScaleZ = MaindictionaryProperties ["z_Scale"];
 	}
 
-	void ApplyBackedUpStats(bool apply = false){
+	public void ApplyBackedUpStats(bool apply = false){
 		MaindictionaryProperties ["Mass_Rigid"] = B_UP_mass;
 		MaindictionaryProperties ["drag_Rigid"] = B_UP_drag;
 		MaindictionaryProperties ["AngularDrag_Rigid"] = B_UP_angularDrag;
 
-		MaindictionaryProperties ["ScaleX_Transform"] = B_UP_scaleX;
-		MaindictionaryProperties ["ScaleY_Transform"] = B_UP_scaleY;
-		MaindictionaryProperties ["ScaleZ_Transform"] = B_UP_scaleZ;
+		MaindictionaryProperties ["x_Transform"] = B_UP_TransformX;
+		MaindictionaryProperties ["y_Transform"] = B_UP_TransformY;
+		MaindictionaryProperties ["z_Transform"] = B_UP_TransformZ;
+
+		MaindictionaryProperties ["x_Scale"] = B_UP_ScaleX;
+		MaindictionaryProperties ["y_Scale"] = B_UP_ScaleY;
+		MaindictionaryProperties ["z_Scale"] = B_UP_ScaleZ;
 
 		if (apply == true) {
 			applyPropertiesOnBall ();
 		}
 	}
 
-	void initializeMaindictionaryProperties(){
+	public void initializeMaindictionaryProperties(){
 		MaindictionaryProperties.Add ("Mass_Rigid", 0);
 		MaindictionaryProperties.Add ("drag_Rigid", 0);
 		MaindictionaryProperties.Add ("AngularDrag_Rigid", 0);
-		MaindictionaryProperties.Add ("ScaleX_Transform", 0);
-		MaindictionaryProperties.Add ("ScaleY_Transform", 0);
-		MaindictionaryProperties.Add ("ScaleZ_Transform", 0);
+		MaindictionaryProperties.Add ("x_Transform", 0);
+		MaindictionaryProperties.Add ("y_Transform", 0);
+		MaindictionaryProperties.Add ("z_Transform", 0);
+		MaindictionaryProperties.Add ("x_Scale", 0);
+		MaindictionaryProperties.Add ("y_Scale", 0);
+		MaindictionaryProperties.Add ("z_Scale", 0);
+
 	
 	}
 
@@ -63,87 +84,142 @@ public class BallProperties : MonoBehaviour {
 	void Update () {
 	}
 
-	float getMass(){
+	public float getMass(){
 		return MaindictionaryProperties["Mass_Rigid"];
 	}
 
 	[System.Obsolete("Method setMass is Deprecated, please use setAndApplyPropertiesOnBall instead")]
-	void setMass(float incomingMass){
+	public void setMass(float incomingMass){
 		MaindictionaryProperties["Mass_Rigid"] = incomingMass;
 	}
 
-	float getDrag(){
+	public float getDrag(){
 		return MaindictionaryProperties["drag_Rigid"];
 	}
 	[System.Obsolete("Method setDrag is Deprecated, please use setAndApplyPropertiesOnBall instead")]
-	void setDrag(float incomingDrag){
+	public void setDrag(float incomingDrag){
 		MaindictionaryProperties["drag_Rigid"] = incomingDrag;
 	}
 
-	float getAngularDrag(){
+	public float getAngularDrag(){
 		return MaindictionaryProperties["AngularDrag_Rigid"];
 	}
 
 	[System.Obsolete("Method setAngularDrag is Deprecated, please use setAndApplyPropertiesOnBall instead")]
-	void setAngularDrag(float incomingAngularDrag){
+	public void setAngularDrag(float incomingAngularDrag){
 		MaindictionaryProperties["AngularDrag_Rigid"] = incomingAngularDrag;
 	}
 
-	float getScaleX(){
-		return MaindictionaryProperties["ScaleX_Transform"];
+	public float getTransformX(){
+		return MaindictionaryProperties["x_Transform"];
 	}
 
-	float getScaleY(){
-		return MaindictionaryProperties["ScaleY_Transform"];
+	public float getTransformY(){
+		return MaindictionaryProperties["y_Transform"];
 	}
 
-	float getScaleZ(){
-		return MaindictionaryProperties["ScaleZ_Transform"];
+	public float getTransformZ(){
+		return MaindictionaryProperties["z_Transform"];
 	} 
 
+	public float getScaleX(){
+		return MaindictionaryProperties["x_Scale"];
+	}
+
+	public float getScaleY(){
+		return MaindictionaryProperties["y_Scale"];
+	}
+
+	public float getScaleZ(){
+		return MaindictionaryProperties["z_Scale"];
+	}
+
 	[System.Obsolete("Method setScaleX is Deprecated, please use setAndApplyPropertiesOnBall instead")]
-	void setScaleX(float incomingScaleX){
-		MaindictionaryProperties["ScaleX_Transform"] = incomingScaleX;
+	public void setScaleX(float incomingScaleX){
+		MaindictionaryProperties["x_Scale"] = incomingScaleX;
 	}
 
 	[System.Obsolete("Method setScaleY is Deprecated, please use setAndApplyPropertiesOnBall instead")]
-	void setScaleY(float incomingScaleY){
-		MaindictionaryProperties["ScaleY_Transform"] = incomingScaleY;
+	public void setScaleY(float incomingScaleY){
+		MaindictionaryProperties["y_Scale"] = incomingScaleY;
 	}
 
 	[System.Obsolete("Method setScaleZ is Deprecated, please use setAndApplyPropertiesOnBall instead")]
-	void setScaleZ(float incomingScaleZ){
-		MaindictionaryProperties["ScaleZ_Transform"] = incomingScaleZ;
+	public void setScaleZ(float incomingScaleZ){
+		MaindictionaryProperties["z_Scale"] = incomingScaleZ;
 	}
 
-	void applyPropertiesOnBall(){
-		ballRigidBody = gameObject.GetComponent<Rigidbody> ();
+	[System.Obsolete("Method setTransformX is Deprecated, please use setAndApplyPropertiesOnBall instead")]
+	public void setTransformX(float incomingTransformX){
+		MaindictionaryProperties["x_Transform"] = incomingTransformX;
+	}
+
+	[System.Obsolete("Method setTransformY is Deprecated, please use setAndApplyPropertiesOnBall instead")]
+	public void setTransformY(float incomingTransformY){
+		MaindictionaryProperties["y_Transform"] = incomingTransformY;
+	}
+
+	[System.Obsolete("Method setTransformZ is Deprecated, please use setAndApplyPropertiesOnBall instead")]
+	public void setTransformZ(float incomingTransformZ){
+		MaindictionaryProperties["z_Transform"] = incomingTransformZ;
+	}
+
+	public void initializeGameObject(){
+		//Debug.Log (CurrentGameObject.GetComponent<Rigidbody> ().mass);
+		setAndApplyPropertiesOnBall (incomingMass_Rigid: CurrentGameObject.GetComponent<Rigidbody> ().mass, incomingdrag_Rigid: CurrentGameObject.GetComponent<Rigidbody> ().drag,
+			incomingAngularDrag_Rigid: CurrentGameObject.GetComponent<Rigidbody> ().angularDrag, incoming_x_Transform: CurrentGameObject.GetComponent<Transform> ().position.x,
+			incoming_y_Transform: CurrentGameObject.GetComponent<Transform> ().position.y, incoming_z_Transform: CurrentGameObject.GetComponent<Transform> ().position.z,
+			incoming_x_Scale:CurrentGameObject.GetComponent<Transform>().localScale.x, incoming_y_Scale:CurrentGameObject.GetComponent<Transform>().localScale.y,
+			incoming_z_Scale:CurrentGameObject.GetComponent<Transform>().localScale.z, apply: false);
+	}
+
+	public void printAllVariables(){
+		List<string> dickeys = new List<string> (MaindictionaryProperties.Keys);
+		for(int i = 0; i < dickeys.Count; i++){
+			Debug.Log(dickeys[i].ToString() + " : " + MaindictionaryProperties[dickeys[i]].ToString());
+		}
+	}
+
+	public void applyPropertiesOnBall(){
+		ballRigidBody = CurrentGameObject.GetComponent<Rigidbody> ();
 		ballRigidBody.mass = MaindictionaryProperties["Mass_Rigid"];
 		ballRigidBody.drag = MaindictionaryProperties["drag_Rigid"];
 		ballRigidBody.angularDrag = MaindictionaryProperties["AngularDrag_Rigid"];
 
-		ballTransform = gameObject.GetComponent<Transform> ();
-		ballTransform.localScale = new Vector3 (MaindictionaryProperties["ScaleX_Transform"], MaindictionaryProperties["ScaleY_Transform"], MaindictionaryProperties["ScaleZ_Transform"]);
+		ballTransform = CurrentGameObject.GetComponent<Transform> ();
+		ballTransform.localScale += new Vector3 (MaindictionaryProperties["x_Scale"], MaindictionaryProperties["y_Scale"], MaindictionaryProperties["z_Scale"]);
+		//ballTransform. = new Vector3 (MaindictionaryProperties["x_Transform"], MaindictionaryProperties["y_Transform"], MaindictionaryProperties["z_Transform"]);
 		//Debug.Log (gameObject.GetComponent<Rigidbody> ().mass.ToString ());
 
 	}
 
-	void setAndApplyPropertiesOnBall
+	public void modifyScale(float scaleX, float scaleY, float scaleZ){
+		ballTransform = CurrentGameObject.GetComponent<Transform> ();
+		ballTransform.localScale += new Vector3 (scaleX, scaleY, scaleZ);
+	}
+
+	public void setAndApplyPropertiesOnBall
 	   (float incomingMass_Rigid= float.NaN,
 		float incomingdrag_Rigid = float.NaN,
 		float incomingAngularDrag_Rigid = float.NaN,
-		float incomingScaleX_Transform = float.NaN,
-		float incomingScaleY_Transform = float.NaN,
-		float incomingScaleZ_Transform = float.NaN,
+		float incoming_x_Transform = float.NaN,
+		float incoming_y_Transform = float.NaN,
+		float incoming_z_Transform = float.NaN,
+		float incoming_x_Scale = float.NaN,
+		float incoming_y_Scale = float.NaN,
+		float incoming_z_Scale = float.NaN,
 		bool apply = false)
 	{
 		Dictionary<string, float> tempdictionaryProperties = new Dictionary<string, float> ();
 		tempdictionaryProperties.Add ("Mass_Rigid", incomingMass_Rigid);
 		tempdictionaryProperties.Add ("drag_Rigid", incomingdrag_Rigid);
 		tempdictionaryProperties.Add ("AngularDrag_Rigid", incomingAngularDrag_Rigid);
-		tempdictionaryProperties.Add ("ScaleX_Transform", incomingScaleX_Transform);
-		tempdictionaryProperties.Add ("ScaleY_Transform", incomingScaleY_Transform);
-		tempdictionaryProperties.Add ("ScaleZ_Transform", incomingScaleZ_Transform);
+		tempdictionaryProperties.Add ("x_Transform", incoming_x_Transform);
+		tempdictionaryProperties.Add ("y_Transform", incoming_y_Transform);
+		tempdictionaryProperties.Add ("z_Transform", incoming_z_Transform);
+		tempdictionaryProperties.Add ("x_Scale", incoming_x_Scale);
+		tempdictionaryProperties.Add ("y_Scale", incoming_y_Scale);
+		tempdictionaryProperties.Add ("z_Scale", incoming_z_Scale);
 
 		
 
@@ -153,7 +229,7 @@ public class BallProperties : MonoBehaviour {
 			if (float.IsNaN (tempdictionaryProperties [dicKeys [i]]) == false) {
 				MaindictionaryProperties [dicKeys [i]] = tempdictionaryProperties [dicKeys [i]];
 			}
-			Debug.Log (MaindictionaryProperties [dicKeys [i]]  +  dicKeys [i]);
+			//Debug.Log (MaindictionaryProperties [dicKeys [i]]  +  dicKeys [i]);
 		}
 		if (apply == true) {
 			applyPropertiesOnBall ();
---
+++ Assets/Scripts/Debuffs/DebuffManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DebuffManager : MonoBehaviour {
+	BallProperties ballPropertiesFunctions = new BallProperties();
+	private float targetTime = 5.0f;
+	private bool startTimer = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (startTimer == true) {
+			targetTime -= Time.deltaTime;
+		}
+		if (targetTime <= 0.0f) {
+			ballPropertiesFunctions.modifyScale (-ballPropertiesFunctions.getScaleX (), -ballPropertiesFunctions.getScaleY (), -ballPropertiesFunctions.getScaleZ ());
+			startTimer = false;
+			targetTime = 5.0f;
+		}
+		//Debug.Log (gameObject.name);
+	}
+		
+
+	void OnTriggerEnter(Collider otherGameObject){
+		//Debug.Log ();
+		string typeOfDebuff = gameObject.name.ToString();
+
+		switch (typeOfDebuff) {
+		case "Double Size Debuff":
+			//initialize instance of the properties of the ball.
+			ballPropertiesFunctions.setGameObject (incomingGameObject: otherGameObject.gameObject);
+			ballPropertiesFunctions.initializeGameObject ();
+			//ballPropertiesFunctions.backUpAllStats ();
+			//Get current Scale.
+			float scaleX = ballPropertiesFunctions.getScaleX ();
+			float scaleY = ballPropertiesFunctions.getScaleY ();
+			float scaleZ = ballPropertiesFunctions.getScaleZ ();
+
+			ballPropertiesFunctions.modifyScale (scaleX, scaleY, scaleZ);
+
+			
+			startTimer = true;
+
+				
+			//ballPropertiesFunctions.printAllVariables ();
+
+			//Debug.Log (scaleX);
+			//Debug.Log (scaleY);
+			//Debug.Log (scaleZ);
+			//Debug.Log (ballPropertiesScript.initializeGameObject(otherGameObject));
+			break;
+			
+		}
+	}
+}
